b0318b9128b6b0357b78e077d01fcfd9423f372b
Adieuwhine <63955568+Adieuwhine@users.noreply.github.com>
2023-11-16 12:03:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let order = document.GetElementById("order");
 buy.addEventListener("click", () => {
   document.getElementById("main").style.display = "none";
   document.getElementById("form").style.display = "block";
-  document.getElementById("user_name").value = tg.initDataUnsafe.first_name + " " + tg.initDataUnsafe.last_name;
+  //document.getElementById("user_name").value = tg.initDataUnsafe.first_name + " " + tg.initDataUnsafe.last_name;
 });
 
 //order.addEventListener("click", () => {
